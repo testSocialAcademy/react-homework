d56b667b233e717c91895c25cf446d5b83344ac0
Serhii <srh.kravchenko@gmail.com>
2016-09-29 21:15:52 +0300

    homework #17

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var my_news = [
     {
         author: 'Саша Печкин',
@@ -15,3 +17,184 @@ var my_news = [
         bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
     }
 ];
+
+window.ee = new EventEmitter();
+
+var Article = React.createClass({
+    propTypes: {
+        data: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    },
+    getInitialState: function () {
+        return {
+            visible: false
+        };
+    },
+    readmoreClick: function (e) {
+        e.preventDefault();
+        this.setState({visible: true});
+    },
+    render: function () {
+        var author = this.props.data.author,
+            text = this.props.data.text,
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
+
+        return (
+            <div className='article'>
+                <p className='news__author'>{author}:</p>
+                <p className='news__text'>{text}</p>
+                <a href="#"
+                   onClick={this.readmoreClick}
+                   className={'news__readmore ' + (visible ? 'none' : '')}>
+                    Подробнее
+                </a>
+                <p className={'news__big-text ' + (visible ? '' : 'none')}>{bigText}</p>
+            </div>
+        )
+    }
+});
+
+var News = React.createClass({
+    propTypes: {
+        data: React.PropTypes.array.isRequired
+    },
+    getInitialState: function () {
+        return {
+            counter: 0
+        }
+    },
+    render: function () {
+        var data = this.props.data;
+        var newsTemplate;
+
+        if (data.length > 0) {
+            newsTemplate = data.map(function (item, index) {
+                return (
+                    <div key={index}>
+                        <Article data={item}/>
+                    </div>
+                )
+            })
+        } else {
+            newsTemplate = <p>К сожалению новостей нет</p>
+        }
+
+        return (
+            <div className='news'>
+                {newsTemplate}
+                <strong
+                    className={'news__count ' + (data.length > 0 ? '' : 'none') }>Всего новостей: {data.length}</strong>
+            </div>
+        );
+    }
+});
+
+var Add = React.createClass({
+    getInitialState: function () {
+        return {
+            authorIsEmpty: true,
+            textIsEmpty: true
+        };
+    },
+    componentDidMount: function () {
+        ReactDOM.findDOMNode(this.refs.author).focus();
+    },
+    onBtnClickHandler: function (e) {
+        e.preventDefault();
+        var textEl = ReactDOM.findDOMNode(this.refs.text);
+        var authorEl = ReactDOM.findDOMNode(this.refs.author);
+
+        var text = textEl.value;
+        var author = authorEl.value;
+        var item = [{
+            author: author,
+            text: text,
+            bigText: 'Очень большой текст'
+        }];
+
+        window.ee.emit('News.add', item);
+
+
+        textEl.value = '';
+        authorEl.value = '';
+
+        this.setState(
+            {textIsEmpty: true},
+            {authorEl: true}
+            );
+
+    },
+    onFieldChange: function (fieldName, e) {
+        if (e.target.value.trim().length > 0) {
+            this.setState({['' + fieldName]: false})
+        } else {
+            this.setState({['' + fieldName]: true})
+        }
+    },
+    render: function () {
+        var authorIsEmpty = this.state.authorIsEmpty,
+            textIsEmpty = this.state.textIsEmpty;
+        return (
+            <form className='add cf'>
+                <input
+                    type='text'
+                    className='add__author'
+                    onChange={this.onFieldChange.bind(this, 'authorIsEmpty')}
+                    placeholder='Имя'
+                    ref='author'
+                />
+                <textarea
+                    className='add__text'
+                    onChange={this.onFieldChange.bind(this, 'textIsEmpty')}
+                    placeholder='Текст'
+                    ref='text'
+                ></textarea>
+
+                <button
+                    className='add__btn'
+                    onClick={this.onBtnClickHandler}
+                    ref='alert_button'
+                    disabled={authorIsEmpty || textIsEmpty}
+                >
+                    Опубликовать
+                </button>
+            </form>
+        );
+    }
+});
+
+var App = React.createClass({
+    getInitialState: function () {
+        return {
+            news: my_news
+        };
+    },
+    componentDidMount: function () {
+        var self = this;
+        window.ee.addListener('News.add', function (item) {
+            var nextNews = item.concat(self.state.news);
+            self.setState({news: nextNews});
+        });
+    },
+    componentWillUnmount: function () {
+        window.ee.removeListener('News.add');
+    },
+    render: function () {
+        return (
+            <div className='app'>
+                <Add />
+                <h3>Новости</h3>
+                <News data={this.state.news}/>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
